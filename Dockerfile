0060a7dc07085f7a9b67c429c0ac627c9749be29
taapesh <starmansix@gmail.com>
2017-09-10 02:26:52 -0500

    added docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM node:8
 
-RUN useradd --user-group --create-home --shell /bin/false app
-
 ENV NPM_CONFIG_LOGLEVEL warn
 
 RUN npm install pm2 -g
@@ -12,7 +10,6 @@ COPY package.json .
 COPY ecosystem.config.js .
 
 # Install app dependencies
-
 RUN npm install --production
 
 CMD [ "npm", "run", "dev"]
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'vsnet-social',
-      script: './src/index.js',
+      script: './src/server.js',
       exec_mode: 'cluster',
       instances: 0,
       wait_ready: true,
       listen_timeout: 10000,
       env: {
         NODE_ENV: 'development',
-        PUBSUB_URL: 'redis://vs-social-redis-pubsub:6379',
         PORT: 3000
       },
       env_production: {
